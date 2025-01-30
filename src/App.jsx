16598477e625f54b293a7118e2181fc1c621c6bb
Vitalii Radchenko <shinevit@gmail.com>
2025-01-31 01:43:24 +0200

    WIP Migration to TailwindCSS theme from tailwind.config.js file

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
-export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world
-    </h1>
-  )
-}
+const App = () => (
+  <main className="relative">
+    Nav
+    <section className="xl:padding-l wide:padding-r padding-b">
+      Hero
+    </section>
+    <section className="padding">
+      PopularProducts
+    </section>
+    <section className="padding">
+      SuperQuality
+    </section>
+    <section className="padding-x py-10">
+      Services
+    </section>
+    <section className="padding">
+      SpecialOffer
+    </section>
+    <section className="bg-pale-blue padding">
+      CustomerReviews
+    </section>
+    <section className="padding-x">
+      Subscribe
+    </section>
+    <section className="padding">
+      Footer
+    </section>
+  </main>
+);
+
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
   theme: {
     fontSize: {
       xs: ['12px', '16px'],
