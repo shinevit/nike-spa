2c928b587681ed6e784ff14bf51110fb7118b011
Vitalii Radchenko <shinevit@gmail.com>
2025-02-01 13:25:31 +0200

    Page structure with components on it is created

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,41 @@
+import {
+  Hero,
+  PopularProducts,
+  SuperQuality,
+  Services,
+  SpecialOffer,
+  CustomerReviews,
+  Subscribe,
+  Footer
+} from './sections';
+
+
 const App = () => (
   <main className="relative">
-    Nav
+    {/* <Nav /> */}
     <section className="xl:padding-l wide:padding-r padding-b">
-      Hero
+      <Hero />
     </section>
     <section className="padding">
-      PopularProducts
+      <PopularProducts />
     </section>
     <section className="padding">
-      SuperQuality
+      <SuperQuality />
     </section>
     <section className="padding-x py-10">
-      Services
+      <Services />
     </section>
     <section className="padding">
-      SpecialOffer
+      <SpecialOffer />
     </section>
     <section className="bg-pale-blue padding">
-      CustomerReviews
+      <CustomerReviews />
     </section>
-    <section className="padding-x">
-      Subscribe
+    <section className="padding-x sm:py-32 py-16 w-full">
+      <Subscribe />
     </section>
-    <section className="padding">
-      Footer
+    <section className="bg-black padding-x padding-y pb-8">
+      <Footer />
     </section>
   </main>
 );
