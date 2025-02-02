817ef2e142d4a281eadeeb941efa59e4f1620f78
Vitalii Radchenko <shinevit@gmail.com>
2025-02-02 14:21:53 +0200

    Nav componenet is implemented
    * Navbar menu items are saved to a constants/index.js file

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,12 @@ import {
   Subscribe,
   Footer
 } from './sections';
+import Nav from './components/Nav';
 
 
 const App = () => (
   <main className="relative">
-    {/* <Nav /> */}
+    <Nav />
     <section className="xl:padding-l wide:padding-r padding-b">
       <Hero />
     </section>
