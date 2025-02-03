2009bc933baaf1c06a1108e1393416163610d907
Vitalii Radchenko <shinevit@gmail.com>
2025-02-03 21:33:14 +0200

    PopularProducts section is completed
    * components index file is added
    * thumbnails of hero cards are fixed a bit

---
+++ src/components/index.js
@@ -0,0 +1,12 @@
+import Nav from "./Nav";
+import CTAButton from "./CTAButton";
+import ShoeCard from "./ShoeCard";
+import PopularProductCard from "./PopularProductCard";
+
+
+export {
+    Nav,
+    CTAButton,
+    ShoeCard,
+    PopularProductCard
+};
